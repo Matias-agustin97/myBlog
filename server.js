01eebd605ac7034b66f2029f8f0e404376b71ef6
Matias Cuervo <matias.a.acuervo@gmail.com>
2021-04-04 07:05:05 -0300

    asdas

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express= require('express')
 const app= express()
-const PORT= process.env.PORT
+const PORT= process.env.PORT || 3000
 const path= require('path')
 const mongoose= require('mongoose')
 const Blog= require('./models/blog.js')
